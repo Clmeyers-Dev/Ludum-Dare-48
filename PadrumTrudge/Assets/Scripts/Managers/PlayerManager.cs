b72cdd4d127dfb9c207b893457fc0c4ae5cb6865
clmeyrs <70337853+Clmeyers-Dev@users.noreply.github.com>
2022-03-08 06:24:48 -0500

    adds  light that scales with HP

--- PadrumTrudge/Assets/Scripts/Managers/PlayerManager.cs
+++ PadrumTrudge/Assets/Scripts/Managers/PlayerManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Experimental.Rendering.Universal;
 public class PlayerManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+   private Light2D Flame;
+   public float Scale;
     void Start()
     {
         
@@ -13,6 +16,6 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Flame.intensity = Scale;
     }
 }
